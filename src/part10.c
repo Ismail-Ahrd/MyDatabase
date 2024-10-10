6222c60f58dadefc3739e2ca871b7fcca75a6472
Ismail_Ahrd <ahroudismail9@gmail.com>
2024-10-10 01:55:45 +0100

    fix an error in part10.c and add unit test for this part

--- src/part10.c
+++ src/part10.c
@@ -250,37 +250,6 @@ void indent(uint32_t level) {
   }
 }
 
-void print_tree(Pager* pager, uint32_t page_num, uint32_t indentation_level) {
-  void* node = get_page(pager, page_num);
-  uint32_t num_keys, child;
-
-  switch (get_node_type(node)) {
-    case (NODE_LEAF):
-      num_keys = *leaf_node_num_cells(node);
-      indent(indentation_level);
-      printf("- leaf (size %d)\n", num_keys);
-      for (uint32_t i = 0; i < num_keys; i++) {
-        indent(indentation_level + 1);
-        printf("- %d\n", *leaf_node_key(node, i));
-      }
-      break;
-    case (NODE_INTERNAL):
-      num_keys = *internal_node_num_keys(node);
-      indent(indentation_level);
-      printf("- internal (size %d)\n", num_keys);
-      for (uint32_t i = 0; i < num_keys; i++) {
-        child = *internal_node_child(node, i);
-        print_tree(pager, child, indentation_level + 1);
-
-        indent(indentation_level + 1);
-        printf("- key %d\n", *internal_node_key(node, i));
-      }
-      child = *internal_node_right_child(node);
-      print_tree(pager, child, indentation_level + 1);
-      break;
-  }
-}
-
 //Convert a row into a flat byte representation suitable for storage.
 void serialize_row(Row* source, void* destination) {
   memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
@@ -329,6 +298,37 @@ void* get_page(Pager* pager, uint32_t page_num) {
   }
 
   return pager->pages[page_num];
+}
+
+void print_tree(Pager* pager, uint32_t page_num, uint32_t indentation_level) {
+  void* node = get_page(pager, page_num);
+  uint32_t num_keys, child;
+
+  switch (get_node_type(node)) {
+    case (NODE_LEAF):
+      num_keys = *leaf_node_num_cells(node);
+      indent(indentation_level);
+      printf("- leaf (size %d)\n", num_keys);
+      for (uint32_t i = 0; i < num_keys; i++) {
+        indent(indentation_level + 1);
+        printf("- %d\n", *leaf_node_key(node, i));
+      }
+      break;
+    case (NODE_INTERNAL):
+      num_keys = *internal_node_num_keys(node);
+      indent(indentation_level);
+      printf("- internal (size %d)\n", num_keys);
+      for (uint32_t i = 0; i < num_keys; i++) {
+        child = *internal_node_child(node, i);
+        print_tree(pager, child, indentation_level + 1);
+
+        indent(indentation_level + 1);
+        printf("- key %d\n", *internal_node_key(node, i));
+      }
+      child = *internal_node_right_child(node);
+      print_tree(pager, child, indentation_level + 1);
+      break;
+  }
 }
 
 
